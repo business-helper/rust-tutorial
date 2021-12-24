0f6d778ccfd28a8a44219ddb1a2195dc82a36d53
Bi Wu <alerk_star@outlook.com>
2021-12-24 15:53:41 +0800

    shadowing from int -> str;

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-    let mut x: i32 = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+  let x: i32 = 5;
+  println!("The value of x is: {}", x);
+  let x: &str = "six";
+  println!("The value of x is: {}", x);
+
+  const SUBSCRIBER_COUNT: u32 = 100_000;
+
 }
