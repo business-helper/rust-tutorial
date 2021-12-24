f1dee322717584069a122a45b54303ec002319f9
Bi Wu <alerk_star@outlook.com>
2021-12-24 16:13:14 +0800

    tuple and arary, destructuring;

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,14 @@ fn main() {
 
   const SUBSCRIBER_COUNT: u32 = 100_000;
 
+  let smile_face: char = '😀';
+
+  let tup: (&str, i32) = ("Let's Get Rusty!😀", 100_000);
+
+  let (channel, sub_count) = tup;
+
+  let error_codes = [200, 404, 500];
+  let not_found: i32 = error_codes[1];
+  
+  let byte = [0; 8];
 }
